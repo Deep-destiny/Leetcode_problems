0a370e84360052c4623a5298f3f5515428fe9700
Deep-destiny <deepakdhakad4445@gmail.com>
2026-08-12 10:56:31 +0530

    Time: 14 ms (71.16%) | Memory: 42.3 MB (74.49%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        boolean []t=new  boolean[n+1];
+        t[0]=false;
+
+        for(int i=1;i<=n;i++){
+            for(int k=1;(k*k)<=i;k++){
+
+                    if(t[i-(k*k)]==false) {
+                        t[i]=true;
+                        break;
+                        
+            }
+        }
+    }
+    return t[n];
+}
+}
+
