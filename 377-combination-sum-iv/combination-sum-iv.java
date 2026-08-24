88cd86beda0a28c0f6e17f9f73c1b10a17d655d6
Deep-destiny <deepakdhakad4445@gmail.com>
2026-08-24 16:44:39 +0530

    Time: 3 ms (13.04%) | Memory: 43.1 MB (8.42%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.java
+++ 377-combination-sum-iv/combination-sum-iv.java
@@ -2,19 +2,21 @@ class Solution {
     public int combinationSum4(int[] nums, int target) {
         int n=nums.length;
         int[][]dp =new int[n+1][target+1];
-        for(int []t:dp){
-        Arrays.fill(t,-1);
-        }
-        return solve(0,target,nums,dp);
-    }
-    int solve(int idx,int target,int[] nums,int[][]dp){
-        if(target==0) return 1;
-          if(idx>=nums.length || target<0) return 0;
-        if(dp[idx][target]!=-1) return dp[idx][target];
-      
-        int take=solve(0,target-nums[idx],nums,dp);
-        int not_take=solve(idx+1,target,nums,dp);
+       
+        for(int i=0;i<=n;i++)dp[i][0]=1;
 
-        return dp[idx][target]=take+not_take;
+       
+            for(int t=1;t<=target;t++){
+                 for(int idx=n-1;idx>=0;idx--){
+                int not_take=dp[idx+1][t];
+
+                int take=0;
+                if(t>=nums[idx]){
+                    take=dp[0][t-nums[idx]];
+                }
+                dp[idx][t]=take+not_take;
+            }
+        }
+        return dp[0][target];
     }
 }
