d277841618353e3c7769fdd851dcb7976603af6f
Deep-destiny <deepakdhakad4445@gmail.com>
2026-09-04 16:22:04 +0530

    Time: 8 ms (91.10%) | Memory: 49.3 MB (14.15%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,23 @@
+    class Solution {
+        public int[][] merge(int[][] intervals) {
+            List<int[]> ans =new ArrayList<>();
+            // int i=0;
+            Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+            int n=intervals.length;
+            int start=intervals[0][0];
+            int end=intervals[0][1];
+            for(int i=1;i<n;i++){
+                // overlapping 
+                if(intervals[i][0]<=end){
+                    end=Math.max(intervals[i][1],end);
+                }
+                else{
+                    ans.add(new int[]{start,end});
+                    start=intervals[i][0];
+                    end=intervals[i][1];
+                }
+            }
+            ans.add(new int[]{start,end});
+            return ans.toArray(new int[ans.size()][]);
+        }
+    }
