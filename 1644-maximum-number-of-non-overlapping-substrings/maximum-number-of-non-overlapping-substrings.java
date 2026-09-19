e63b8f5bb4195371eac95cda883c8909177e433c
Deep-destiny <deepakdhakad4445@gmail.com>
2026-09-19 11:10:07 +0530

    Time: 16 ms (35.40%) | Memory: 47.8 MB (88.11%) - LeetSync

--- 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.java
+++ 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.java
@@ -1,52 +1,44 @@
 class Solution {
     public List<String> maxNumOfSubstrings(String s) {
-        int n = s.length();
-
-        int[] start = new int[26];
-        int[] end = new int[26];
-        boolean[] isValid = new boolean[26];
-
-        Arrays.fill(start, -1);
-        Arrays.fill(isValid, true);
-
-        List<String> result = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            int idx = s.charAt(i) - 'a';
-
-            if (start[idx] == -1) {
-                start[idx] = i;
-            }
-            end[idx] = i;
+     int n=s.length();
+     //step1: find start and end of the each character
+     int[] start=new int[26];
+     Arrays.fill(start,-1);
+     int[] end=new int[26];
+      Arrays.fill(end,-1);
+    for(int i=0;i<n;i++){
+        int idx=s.charAt(i)-'a';
+        if(start[idx]==-1){
+            start[idx]=i;
         }
-
-        for (int c = 0; c < 26; c++) { // checking all characters
-            if (start[c] == -1) continue;
-
-            for (int i = start[c]; i <= end[c]; i++) {
-                int ch = s.charAt(i) - 'a';
-                if (start[ch] < start[c]) {
-                    isValid[c] = false;
-                    break;
-                }
-
-                end[c] = Math.max(end[c], end[ch]);
+        end[idx]=i;
+    }
+    // step 2: Extend each index if possible 
+    //          Mark invalid if possible
+    boolean [] isValid=new boolean [26];
+    Arrays.fill(isValid,true);
+    for(int c=0;c<26;c++){
+        if(start[c]==-1)continue;
+        for(int i=start[c];i<=end[c];i++){
+            if(start[s.charAt(i)-'a']<start[c]){
+                isValid[c]=false;
+                break;
             }
+            end[c]=Math.max(end[c],end[s.charAt(i)-'a']);
         }
-
-        int lastTakenStart = Integer.MAX_VALUE;
-
-        for (int i = n - 1; i >= 0; i--) {
-            int c = s.charAt(i) - 'a';
-
-            if (!isValid[c]) continue;
-
-            if (i == start[c] && end[c] < lastTakenStart) {
-                result.add(s.substring(i, end[c] + 1));
-                lastTakenStart = i;
-            }
+    }
+    // step3: overlapping -> keep track of last substring s+idx
+    //        smaller length substring
+    List<String> list=new ArrayList<>();
+    int lastTakenStart=Integer.MAX_VALUE;
+    for(int i=n-1;i>=0;i--){
+        int idx=s.charAt(i)-'a';
+        if(!isValid[idx])continue;
+        if(i==start[idx] && end[idx]<lastTakenStart){
+            list.add(s.substring(i,end[idx]+1));
+            lastTakenStart=i;
         }
-
-        return result;
+    }
+    return list;
     }
 }
