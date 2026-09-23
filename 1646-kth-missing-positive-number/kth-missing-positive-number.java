57c1a3283416ff027cf5a83a15612ab311e68a8a
Deep-destiny <deepakdhakad4445@gmail.com>
2026-09-23 19:44:15 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (57.32%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -2,16 +2,17 @@ class Solution {
     public int findKthPositive(int[] arr, int k) {
         // using binsry search 
         int n=arr.length;
-        int []missing=new int[n];
-        for(int i=0;i<n;i++){
-            missing[i]=arr[i]-(i+1);
-        }
+        // int []missing=new int[n];
+        // for(int i=0;i<n;i++){
+        //     missing[i]=arr[i]-(i+1);
+        // }
         int l=0;
         int h=arr.length-1;
 
         while(l<=h){
             int mid=l+(h-l)/2;
-            if(missing[mid]<k){
+            int missing=arr[mid]-(mid+1);
+            if(missing<k){
                 l=mid+1;
             }
             else h=mid-1;
