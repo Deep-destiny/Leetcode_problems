4374d469c8faeeabf3962f461ef8f860f279a5da
Deep-destiny <deepakdhakad4445@gmail.com>
2026-09-26 13:50:13 +0530

    Time: 1 ms (83.29%) | Memory: 43.6 MB (79.45%) - LeetSync

---
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res=new ArrayList<>();
+        if(root==null) return res;
+        Queue<TreeNode> q=new LinkedList<>();
+        boolean org=false;
+
+        q.offer(root);
+        while(!q.isEmpty()){
+            List<Integer> list=new ArrayList<>();
+            int levelsize=q.size();
+            for(int i=0;i<levelsize;i++){
+            TreeNode curr=q.poll();
+            list.add(curr.val);
+                if(curr.left!=null){
+                    q.offer(curr.left);
+                }
+                if(curr.right!=null){
+                    q.offer(curr.right);
+            }
+            }
+            if(org) Collections.reverse(list);
+                org=!org;
+                res.add(list);
+        }
+       return res;
+    }
+}
